d5e49b7d4ade69898809e44fcb665bfa10f2da56
Whu-wxy <641645560@qq.com>
2019-02-20 16:08:29 +0800

    fix punctuation_mend

--- punctuation_mend.py
+++ punctuation_mend.py
@@ -4,15 +4,16 @@ def punctuation_mend(string):
 
     table = {ord(f):ord(t) for f,t in zip(
         u'，。！？【】（）％＃＠＆１２３４５６７８９０“”‘’',
-        u',.!?[]()%#@&1234567890""''')}
+        u',.!?[]()%#@&1234567890""\'\'')}
     if os.path.isfile(string):
-        with open(string, 'w+', encoding='utf-8') as f:
+        with open(string, 'r', encoding='utf-8') as f:
             res = unicodedata.normalize('NFKC', f.read())
             res = res.translate(table)
+        with open(string, 'w', encoding='utf-8') as f:
             f.write(res)
     else:
         res = unicodedata.normalize('NFKC', string)
         res = res.translate(table)
         return res
 
-print(punctuation_mend('【】（）％＃＠＆“”'))
+punctuation_mend('F:/z.txt')
