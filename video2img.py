f3f32d7d9c2ae0c8339f89592c55228a14afe6d7
Whu-wxy <641645560@qq.com>
2019-06-27 16:22:30 +0800

    Create video2img.py

---
+++ video2img.py
@@ -0,0 +1,36 @@
+import cv2
+from os import path
+import os
+
+
+file_name1 = r'E:\video\CCTV16396518_1558062062_10.mp4'
+file_name2 = r'E:\video\CCTV16396518_1558062062_11.mp4'
+
+def video2img(file_name, frame_number=60):
+    name = path.basename(file_name).split('.')[0]
+    save_path = path.dirname(file_name) + '\\' + name + '\\'
+    if os.path.exists(save_path) == False:    #在视频同级目录下创建文件夹
+        os.mkdir(save_path)
+
+    vc = cv2.VideoCapture(file_name)  # 读入视频文件
+    c = 1
+
+    if vc.isOpened():  # 判断是否正常打开
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    timeF = frame_number  # 视频帧计数间隔频率
+
+    while rval:  # 循环读取视频帧
+        rval, frame = vc.read()
+        if (c % timeF == 0):  # 每隔timeF帧进行存储操作
+            cv2.imwrite(save_path + str(c) + '.jpg', frame)  # 存储为图像
+        c = c + 1
+        cv2.waitKey(1)
+    vc.release()
+
+# video2img(file_name1)
+video2img(file_name2)
+
+print('All Finished!')
